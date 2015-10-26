d7ce0d71d89a7961ccd1410945398a2b5457df9c
Paulo Silva <ster1177@gmail.com>
2015-10-26 17:46:57 +0000

    Inimigos com animação

--- Assets/Scripts/hit.cs
+++ Assets/Scripts/hit.cs
@@ -2,10 +2,24 @@
 using System.Collections;
 
 public class hit : MonoBehaviour {
+
+	//public GameObject bearBody;
+	public float thrust = 5;
+
 	void OnTriggerEnter(Collider other) {
 		if (other.CompareTag ("Enemy")) {
 			Debug.Log ("ACERTOU URSO");
-			Destroy (other.gameObject);
+			//Destroy (other.gameObject);
+			//if(other.gameObject.transform.Rotate.z >= 0) 
+				//other.gameObject.transform.Rotate(0,0,30);
+			GameObject bear = other.gameObject;
+
+			bear.GetComponentInChildren<Animator>().SetTrigger("die");
+			/*bearBody = other.gameObject.GetComponent<Rigidbody>();
+			 
+			bearBody.AddForce(this.GetComponent<Rigidbody>().velocity.normalized*thrust, ForceMode.Impulse);*/
+			//other.gameObject.transform.position -= transform.forward * Time.deltaTime * 5;
+
 		} else if(!other.CompareTag("Ignore"))
 			Destroy (this.gameObject);
 	}
