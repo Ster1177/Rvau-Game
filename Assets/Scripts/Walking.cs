8e7a98caa675aafad59c0c1b2790d684c4cdf5c4
Paulo Silva <ster1177@gmail.com>
2015-11-05 22:32:44 +0000

    Reorganizar mambos

---
+++ Assets/Scripts/Walking.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walking : MonoBehaviour {
+	private NavMeshAgent agent;
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination (target.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
