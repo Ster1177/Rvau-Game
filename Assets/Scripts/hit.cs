20b9e7bd8a198a1e250b0b225bbc519fbfff418f
Daniel Teixeira <daniel-hernani@sapo.pt>
2015-11-09 15:11:19 +0000

    terrain works (sort of)

--- Assets/Scripts/hit.cs
+++ Assets/Scripts/hit.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI; 
 
 public class hit : MonoBehaviour {
 
 	//public GameObject bearBody;
 	public float thrust = 5;
 	public  float dieTime = 1.2f;
+	public int score = 0;
+	private Text txt;
 
 	void OnTriggerEnter(Collider other) {
 	//	Debug.Log ("ACERTOU URSO");
+		GameObject scoreBoard = GameObject.FindWithTag("Score");
 	
 		
 			//Destroy (other.gameObject);
@@ -18,6 +22,10 @@ public class hit : MonoBehaviour {
 	//	Debug.Log ("Eu sou um: " + gameObject.name);
 		if (other.gameObject.name == "Bullet") {
 		//	Debug.Log("Morre Diabo");
+			score+= 5;
+			txt = scoreBoard.GetComponent<Text>();
+			txt.text = score.ToString();
+
 			transform.GetComponentInChildren<Animator>().SetTrigger ("die");
 			Destroy(gameObject,dieTime);
 		}
